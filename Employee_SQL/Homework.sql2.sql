48e2a1e62da858c361c1d393565e20eaffaf6935
Beccapwd <rjstilley@gmail.com>
2021-03-15 19:28:12 -0500

    added numbers to questions

--- Employee_SQL/Homework.sql2.sql
+++ Employee_SQL/Homework.sql2.sql
@@ -5,29 +5,29 @@ select * from employees;
 select * from salaries;
 select * from titles;
 
---List the following details of each employee: employee number, last name, first name, sex, and salary.
+-- 1.List the following details of each employee: employee number, last name, first name, sex, and salary.
 select employees.emp_no, employees.first_name, employees.last_name, employees.sex, salaries.salary from employees
 	inner join salaries on employees.emp_no = salaries.emp_no order by emp_no;
 
---List first name, last name, and hire date for employees who were hired in 1986.
+-- 2.List first name, last name, and hire date for employees who were hired in 1986.
 select first_name, last_name, hire_date from employees
 where hire_date between '1986-01-01' and '1986-12-31' order by hire_date;
 
---List the manager of each department with the following information: department number, department name, the manager's employee number, last name, first name.
+-- 3.List the manager of each department with the following information: department number, department name, the manager's employee number, last name, first name.
 select d.dept_no, d.dept_name, dm.emp_no, e.last_name, e.first_name from departments d
 	inner join dept_managers dm on d.dept_no = dm.dept_no
 	inner join employees e on dm.emp_no = e.emp_no order by emp_no;
 
---List the department of each employee with the following information: employee number, last name, first name, and department name.
+-- 4.List the department of each employee with the following information: employee number, last name, first name, and department name.
 
 
---List first name, last name, and sex for employees whose first name is "Hercules" and last names begin with "B."
+-- 5.List first name, last name, and sex for employees whose first name is "Hercules" and last names begin with "B."
 
 
---List all employees in the Sales department, including their employee number, last name, first name, and department name.
+-- 6.List all employees in the Sales department, including their employee number, last name, first name, and department name.
 
 
---List all employees in the Sales and Development departments, including their employee number, last name, first name, and department name.
+-- 7.List all employees in the Sales and Development departments, including their employee number, last name, first name, and department name.
 
 
---In descending order, list the frequency count of employee last names, i.e., how many employees share each last name.
+-- 8.In descending order, list the frequency count of employee last names, i.e., how many employees share each last name.
